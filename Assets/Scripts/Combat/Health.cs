4308598c3ff242441f3a943b120f103421565078
Nicole Maia <nicolemaia@MacBookPro.attlocal.net>
2026-09-25 01:42:33 -0400

    Add basic health and damage system

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 100f;
+    private float currentHealth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Took " + damage + " damage. Health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log(gameObject.name + "died.");
+        Destroy(gameObject);
+    }
+}
